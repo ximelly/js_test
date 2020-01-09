4f459d2f70a2d65251278d7a8d3dd0abae46c148
ximelly <ximelly@163.com>
2020-01-09 16:09:29 +0800

    使用jest
    使用jest

---
+++ jest/index.js
@@ -0,0 +1,18 @@
+function add(a,b){
+    return a+b;
+}
+function reduce(a,b){
+    return a-b;
+}
+function multy(a,b){
+    return a*b;
+}
+try{
+    module.exports={
+        add,
+        reduce,
+        multy
+    }
+}catch(e){
+
+}
---
+++ jest/index.test.js
@@ -0,0 +1,10 @@
+const {add,reduce,multy}=require("./index.js");
+test("测试加法",()=>{
+    expect(add(1,2)).toBe(3);
+});
+test("测试建法",()=>{
+    expect(reduce(1,2)).toBe(-1);
+});
+test("测试乘法",()=>{
+    expect(multy(2,3)).toBe(6);
+});

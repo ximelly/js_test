f0cd475f072acd6824755b2d95a78c3d54034128
ximelly <ximelly@163.com>
2020-01-14 15:41:59 +0800

    describe的钩子函数
    describe的钩子函数

--- lifeCycle/count.test.js
+++ lifeCycle/count.test.js
@@ -1,41 +1,51 @@
 import Counter from "./count"
 
-let count=null;
-beforeAll(()=>{
-  console.log("beforeAll");
-})
-beforeEach(()=>{
-  console.log("beforeEach");
-  count=new Counter();
-})
-afterEach(()=>{
-  console.log("afterEach");
-})
-afterAll(()=>{
-  console.log("afterAll");
-})
+//每个describe都可以有自己的钩子函数
+describe("测试count",()=>{
+  let count=null;
+  beforeAll(()=>{
+    console.log("beforeAll");
+  })
+  beforeEach(()=>{
+    console.log("beforeEach");
+    count=new Counter();
+  })
+  afterEach(()=>{
+    console.log("afterEach");
+  })
+  afterAll(()=>{
+    console.log("afterAll");
+  })
 
-describe("测试add相关代码",()=>{
-  test("测试add",()=>{
-    console.log("测试add");
-    count.add();
-    expect(count.number).toBe(1);
-  });
-  test("测试add1",()=>{
-    console.log("测试add1");
-    count.add1();
-    expect(count.number).toBe(2);
-  });
-})
-describe("测试minus相关代码",()=>{
-  test("测试minus",()=>{
-    console.log("测试minus");
-    count.minus();
-    expect(count.number).toBe(-1);
-  });
-  test("测试minus1",()=>{
-    console.log("测试minus1");
-    count.minus1();
-    expect(count.number).toBe(-2);
-  });
+  describe("测试add相关代码",()=>{
+    beforeEach(()=>{
+      console.log("beforeEach inner");
+      count=new Counter();
+    })
+    afterEach(()=>{
+      console.log("afterEach inner");
+    })
+    test("测试add",()=>{
+      console.log("测试add");
+      count.add();
+      expect(count.number).toBe(1);
+    });
+    test("测试add1",()=>{
+      console.log("测试add1");
+      count.add1();
+      expect(count.number).toBe(2);
+    });
+  })
+  describe("测试minus相关代码",()=>{
+    test("测试minus",()=>{
+      console.log("测试minus");
+      count.minus();
+      expect(count.number).toBe(-1);
+    });
+    test("测试minus1",()=>{
+      console.log("测试minus1");
+      count.minus1();
+      expect(count.number).toBe(-2);
+    });
+  })
 })
